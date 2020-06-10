9321c40586c6089cd4a82e91070df837b3a5a8df
kangert <kangert@qq.com>
2020-06-10 11:47:00 +0800

    优化 学生数据导出

--- apps/system/exportStudentData.py
+++ apps/system/exportStudentData.py
@@ -3,7 +3,7 @@ import os
 from django.http import StreamingHttpResponse, JsonResponse
 from xlwt import *
 
-from .models import studentManage, professionManage, classesManage
+from .models import studentManage, professionManage, classesManage, enterprisePost, enterpriseManage
 
 
 def getexportStudentData(request):
@@ -13,67 +13,112 @@ def getexportStudentData(request):
     :return: 返回文件流对象
     """
     searchType = request['getDataType']
-    studentList1 = []  # 组装包含绑定专业及班级的关系
-    studentBaseData = studentManage.objects.filter().values()  # 获取学生基本数据
-    for i in studentBaseData:
-        if studentManage.objects.filter(studentCode=i['studentCode']).count() > 0:
-            for ii in studentManage.objects.filter(studentCode=i['studentCode']).values():  # 获取到绑定关系
-                i.update(ii)
-                studentList1.append(i)
-        else:
-            i.update({'studentCode': i['studentCode'], 'professionCode': '-1', 'classesCode': '-1'})
-            studentList1.append(i)
 
-    # 替换专业代码为专业名称
-    studentList2 = []
-    for iii in studentList1:
-        if str(iii['professionCode']) != '-1':
-            for iiii in professionManage.objects.filter(professionCode=iii['professionCode']).values():
-                iii.update({'professionName': iiii['professionName']})
-                studentList2.append(iii)
-        else:
-            iii.update({'professionName': '未绑定'})
-            studentList2.append(iii)
+    # 创建新的列表供后续功能操作
+    subData0 = []
 
-    # 替换班级代码为班级名称
-    studentList3 = []
-    for student in studentList2:
-        if str(student['classesCode']) != '-1':
-            for classData in classesManage.objects.filter(classesCode=student['classesCode']).values():
-                student.update({'classesLevel': classData['classesLevel'], 'classesName': classData['classesName']})
-                studentList3.append(student)
-        else:
-            student.update({'classesLevel': '未绑定', 'classesName': '未绑定'})
-            studentList3.append(student)
+    obj = studentManage.objects
 
-    # 去掉无用字段组成最终数据
-    studentList = []
-    for studentData in studentList3:
-        studentData.pop('classesCode')
-        studentData.pop('professionCode')
-        studentList.append(studentData)
+    if searchType in ['全部', '参军', '待安置', '已安置', '拟升学']:
 
+        if searchType == '全部':
+            subData0 = list(obj.filter().values())
+
+        if searchType == '参军':
+            subData0 = list(obj.filter(employmentStatus='参军').values())
+
+        if searchType == '待安置':
+            subData0 = list(obj.filter(employmentStatus='待安置').values())
+
+        if searchType == '已安置':
+            subData0 = list(obj.filter(employmentStatus='已安置').values())
+
+        if searchType == '拟升学':
+            subData0 = list(obj.filter(employmentStatus='拟升学').values())
+
+    userList = []
+    for i in subData0:
+        # 获取所属专业,班级名称，班级届数并合并到学生信息列表中
+        for ii in obj.filter(studentCode=i['studentCode']).values():
+            if ii['classesCode'] != '0':
+                studentLevel = ''
+                toClasses = ''
+                toProfession = ''
+                for iii in classesManage.objects.filter(classesCode=ii['classesCode']).values():
+                    studentLevel = iii['classesLevel']
+                    toClasses = iii['classesName']
+                for iiii in professionManage.objects.filter(professionCode=ii['professionCode']).values():
+                    toProfession = iiii['professionName']
+                i.update({'studentLevel': studentLevel, 'toProfession': toProfession, 'toClasses': toClasses})
+            else:
+                i.update({'studentLevel': '未绑定', 'toProfession': '未绑定', 'toClasses': '未绑定'})
+
+        # 获取岗位信息和企业信息
+        for studentBindData in list(obj.filter(studentCode=i['studentCode']).values()):
+            if studentBindData['postCode'] != '0':
+                for postData in list(enterprisePost.objects.filter(postCode=studentBindData['postCode']).values()):
+                    i.update({'postName': postData['postName'], 'postAddress': postData['postAddress']})
+            else:
+                i.update({'postName': '未绑定', 'postAddress': '未绑定'})
+
+            if studentBindData['enterpriseCode'] != '0':
+                for enterpriseData in list(
+                        enterpriseManage.objects.filter(enterpriseCode=studentBindData['enterpriseCode']).values()):
+                    i.update({'enterpriseName': enterpriseData['enterpriseName'],
+                              'enterpriseAddress': enterpriseData['enterpriseAddress'],
+                              'enterprisePhone': enterpriseData['enterprisePhone']})
+            else:
+                i.update({'enterpriseName': '未绑定', 'enterpriseAddress': '未绑定', 'enterprisePhone': '未绑定'})
+        userList.append(i)
+
+    # {
+    #     'studentCode': '30317217',
+    #     'studentName': '方言',
+    #     'studentSex': '男生',
+    #     'studentNativePlace': '陕西省-安康市',
+    #     'studentPhone': '11111111111',
+    #     'employmentStatus': '已安置',
+    #     'studentSalary': 3000,
+    #     'teacherName': '王斌',
+    #     'teacherPhone': '11111111111',
+    #     'studentStatus': '良好',
+    #     'postCode': '1003',
+    #     'enterpriseCode': '1002',
+    #     'postDuty': '软件测试',
+    #     'professionCode': '1000',
+    #     'classesCode': '1000',
+    #     'remarks': '1',
+    #     'addTime': datetime.datetime(),
+    #     'studentLevel': '2018',
+    #     'toProfession': '云计算技术与运用',
+    #     'toClasses': '云算3182',
+    #     'postName': '软件测试',
+    #     'postAddress': '新疆维吾尔自治区-阿勒泰地区',
+    #     'enterpriseName': '飞象互娱科技',
+    #     'enterpriseAddress': '新疆维吾尔自治区阿勒泰',
+    #     'enterprisePhone': '01085518557'
+    # }
 
     excelData = []
     excelTitle = ''
     if searchType == '全部':
-        excelData = studentList
+        excelData = userList
         excelTitle = '中兴创新学院学生就业管理系统(学生全部数据)'
 
     if searchType == '参军':
-        excelData = [i for i in studentList if i['employmentStatus'] == '参军']
+        excelData = [i for i in userList if i['employmentStatus'] == '参军']
         excelTitle = '中兴创新学院学生就业管理系统(参军学生数据)'
 
     if searchType == '待安置':
-        excelData = [i for i in studentList if i['employmentStatus'] == '待安置']
+        excelData = [i for i in userList if i['employmentStatus'] == '待安置']
         excelTitle = '中兴创新学院学生就业管理系统(待安置学生数据)'
 
     if searchType == '已安置':
-        excelData = [i for i in studentList if i['employmentStatus'] == '已安置']
+        excelData = [i for i in userList if i['employmentStatus'] == '已安置']
         excelTitle = '中兴创新学院学生就业管理系统(已安置学生数据)'
 
     if searchType == '拟升学':
-        excelData = [i for i in studentList if i['employmentStatus'] == '拟升学']
+        excelData = [i for i in userList if i['employmentStatus'] == '拟升学']
         excelTitle = '中兴创新学院学生就业管理系统(拟升学学生数据)'
 
     if len(excelData) > 0:
@@ -120,23 +165,25 @@ def getexportStudentData(request):
 
         # 设置表头
         w.write(1, 0, "序号", style)
-        w.write(1, 1, "学生届数", style)
-        w.write(1, 2, "所属专业", style)
-        w.write(1, 3, "所属班级", style)
-        w.write(1, 4, "学生学号", style)
-        w.write(1, 5, "学生姓名", style)
-        w.write(1, 6, "学生性别", style)
-        w.write(1, 7, "学生电话", style)
-        w.write(1, 8, "就业状态", style)
-        w.write(1, 9, "实习单位名称", style)
-        w.write(1, 10, "实习单位地址", style)
-        w.write(1, 11, "岗位名称", style)
-        w.write(1, 12, "实习薪资", style)
-        w.write(1, 13, "实习单位电话", style)
-        w.write(1, 14, "指导老师姓名", style)
-        w.write(1, 15, "指导老师电话", style)
-        w.write(1, 16, "学生状态", style)
-        w.write(1, 17, "创建时间", style)
+        w.write(1, 1, "学生学号", style)
+        w.write(1, 2, "学生姓名", style)
+        w.write(1, 3, "学生性别", style)
+        w.write(1, 4, "学生届数", style)
+        w.write(1, 5, "所属专业", style)
+        w.write(1, 6, "所属班级", style)
+        w.write(1, 7, "学生籍贯", style)
+        w.write(1, 8, "学生电话", style)
+        w.write(1, 9, "就业状态", style)
+        w.write(1, 10, "企业名称", style)
+        w.write(1, 11, "企业地址", style)
+        w.write(1, 12, "企业电话", style)
+        w.write(1, 13, "岗位名称", style)
+        w.write(1, 14, "工作地址", style)
+        w.write(1, 15, "最新薪资", style)
+        w.write(1, 16, "直属主管", style)
+        w.write(1, 17, "主管电话", style)
+        w.write(1, 18, "学生状态", style)
+        w.write(1, 19, "修改时间", style)
 
         # 写入数据(第一行是标题，第二行表头，第三行开始写数据)
         excel_row = 2
@@ -145,38 +192,46 @@ def getexportStudentData(request):
         index = 0
         for obj in excelData:
             index = index + 1
-            classesLevel = obj['classesLevel']
-            professionName = obj['professionName']
-            classesName = obj['classesName']
-            studentCode = str(obj['studentCode'])
+            studentCode = obj['studentCode']
             studentName = obj['studentName']
             studentSex = obj['studentSex']
+            studentLevel = obj['studentLevel']
+            toProfession = obj['toProfession']
+            toClasses = obj['toClasses']
+            studentNativePlace = obj['studentNativePlace']
             studentPhone = obj['studentPhone']
             employmentStatus = obj['employmentStatus']
-            # companyName = obj['companyName']
-            # companyAddress = obj['companyAddress']
-            # postName = obj['postName']
+            enterpriseName = obj['enterpriseName']
+            enterpriseAddress = obj['enterpriseAddress']
+            enterprisePhone = obj['enterprisePhone']
+            postName = obj['postName']
+            postAddress = obj['postAddress']
             studentSalary = obj['studentSalary']
-            # companyPhone = obj['companyPhone']
             teacherName = obj['teacherName']
             teacherPhone = obj['teacherPhone']
             studentStatus = obj['studentStatus']
-            addTime = obj['addTime'].strftime("%Y-%m-%d")[:10]
+            addTime = obj['addTime'].strftime("%Y-%m-%d %H:%M:%S")[:19]
 
             w.write(excel_row, 0, index, style)
-            w.write(excel_row, 1, classesLevel, style)
-            w.write(excel_row, 2, professionName, style)
-            w.write(excel_row, 3, classesName, style)
-            w.write(excel_row, 4, studentCode, style)
-            w.write(excel_row, 5, studentName, style)
-            w.write(excel_row, 6, studentSex, style)
-            w.write(excel_row, 7, studentPhone, style)
-            w.write(excel_row, 8, employmentStatus, style)
-            w.write(excel_row, 12, studentSalary, style)
-            w.write(excel_row, 14, teacherName, style)
-            w.write(excel_row, 15, teacherPhone, style)
-            w.write(excel_row, 16, studentStatus, style)
-            w.write(excel_row, 17, addTime, style)
+            w.write(excel_row, 1, studentCode, style)
+            w.write(excel_row, 2, studentName, style)
+            w.write(excel_row, 3, studentSex, style)
+            w.write(excel_row, 4, studentLevel, style)
+            w.write(excel_row, 5, toProfession, style)
+            w.write(excel_row, 6, toClasses, style)
+            w.write(excel_row, 7, studentNativePlace, style)
+            w.write(excel_row, 8, studentPhone, style)
+            w.write(excel_row, 9, employmentStatus, style)
+            w.write(excel_row, 10, enterpriseName, style)
+            w.write(excel_row, 11, enterpriseAddress, style)
+            w.write(excel_row, 12, enterprisePhone, style)
+            w.write(excel_row, 13, postName, style)
+            w.write(excel_row, 14, postAddress, style)
+            w.write(excel_row, 15, studentSalary, style)
+            w.write(excel_row, 16, teacherName, style)
+            w.write(excel_row, 17, teacherPhone, style)
+            w.write(excel_row, 18, studentStatus, style)
+            w.write(excel_row, 19, addTime, style)
 
             excel_row += 1
         # 检测文件是够存在
